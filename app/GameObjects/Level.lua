1a0da30dca60888992e3b876e521f30d3ce8f843
Robotic-Brain <github@roboticbrain.de>
2015-04-19 07:35:56 +0200

    Fixing namespace derp

--- app/GameObjects/Level.lua
+++ app/GameObjects/Level.lua
@@ -1,14 +1,14 @@
 require("GameObjects.GameObject")
 Level = GameObject:new()
 
-function Player:init()
+function Level:init()
 	GameObject.init(self)
 	self:setName("Level")
 	self.image = love.graphics.newImage("images/Player.png")
 	return self
 end
 
-function Player:onAddedToWorld()
+function Level:onAddedToWorld()
 	GameObject.onAddedToWorld(self)
 	self.collider = Collider:newCircle(10):setOwner(self)
 	self:getWorld().physics:addCollider(self.collider)
